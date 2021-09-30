1bf956cab61f1740fc8c5a9eb7609cdde151041e
Brooks Feldker <brfeldker1@gmail.com>
2021-09-30 01:33:32 -0500

    Added flavor text for .work and filled in missing .results

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -2,8 +2,8 @@ module.exports.run = async (client, message, args, level, Discord, eco) => {
   // Get the starbits emoji
   const starbits = client.emojis.cache.get(client.emoji.starbits);
   // Define characters and jobs arrays
-  const characters = ['Mario', 'Luigi', 'Bowser', 'Peach', 'Yoshi', 'E. Gadd', 'the Koopalings', 'Toad', 'Toadette', 'Cappy', 'Rosalina', 'Boo', 'Goomba', 'Koopa Troopa', 'Koopa the Quick', 'Donkey Kong', 'Daisy', 'Wario', 'Waluigi', 'Shy Guy', 'Chargin Chuck', 'Pyoro', 'Beaoro', 'Bayonetta', 'King K. Rool', 'Funky Kong', 'The Chimp', 'The Champ', 'Cranky Kong', 'Rabbid Peach', 'Judge Pianta', 'Plessie'];
-  const jobs = ['Personal Chef', 'Minion', 'Bodyguard', 'Lawyer', 'Assistant', 'Babysitter', 'Personal Maid', 'Mailman', 'Driver', 'Gardener', 'Lawn Mower', 'Consultant', 'Copyright Lawyer', 'Player 2'];
+  const characters = ['Mario', 'Luigi', 'Bowser', 'Peach', 'Yoshi', 'E. Gadd', 'the Koopalings', 'Toad', 'Toadette', 'Cappy', 'Rosalina', 'Boo', 'Goomba', 'Koopa Troopa', 'Koopa the Quick', 'Donkey Kong', 'Daisy', 'Wario', 'Waluigi', 'Shy Guy', 'Chargin Chuck', 'Pyoro', 'Beaoro', 'Bayonetta', 'King K. Rool', 'Funky Kong', 'The Chimp', 'The Champ', 'Cranky Kong', 'Rabbid Peach', 'Judge Pianta', 'Plessie', 'King Augustus Septemberus Octoberus Koopa', 'Jr. Troopa', 'Wart', 'King Boo', 'Ninji', 'Pauline', 'Tiara', 'the Broodals', 'Jack Black', 'Foreman Spike', 'Diddy Kong', 'Dixie Kong', 'Whomp', 'Pidgit', 'Gooper Blooper', 'Scuttlebug', 'Nabbit\'s Ghost'];
+  const jobs = ['Personal Chef', 'Minion', 'Bodyguard', 'Lawyer', 'Assistant', 'Babysitter', 'Personal Maid', 'Mailman', 'Driver', 'Gardener', 'Lawn-Mower', 'Consultant', 'Copyright Lawyer', 'Player 2', 'Trash Collector', 'Hair Stylist', 'Pet Sitter', 'Hitman', 'Therapist', 'Tour Guide', 'Photographer', 'Jester', 'Partner in Crime', 'DJ'];
 
   // Get a random character and job
   const rChar = Math.floor(Math.random() * characters.length);
