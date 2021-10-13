a4a0eecc691d8f7d68e3c2f037935b270369e246
Brooks Feldker <brfeldker1@gmail.com>
2021-10-13 00:18:20 -0500

    Commented code

--- commands/misc/coordinates.js
+++ commands/misc/coordinates.js
@@ -2,14 +2,16 @@ module.exports.run = async (client, message, args, level, Discord) => {
   // Ensure the factionSettings exists in the Enmap
   message.channel.send('Voting has started! You have 30 seconds to type in a space to hit using the format `[A-J][0-9]`.');
 
+  //Filter for collector: all posts with format [a-j (case independent)][0-9]
   const re = new RegExp('[A-Ja-j][0-9]');
   const filter = post => re.test(post.content.toUpperCase());
 
+  //Collector collects posts that fit filter for 30 seconds
   const collector = message.channel.createMessageCollector({ filter, time: 30000 });
 
   collector.on('collect', post => {
 	console.log(post.content);
-	post.delete();
+	post.delete(); //Deletes post after collection
   });
 
   let values = new Array(100).fill(0);
@@ -17,10 +19,13 @@ module.exports.run = async (client, message, args, level, Discord) => {
   collector.on('end', posts => {
 	posts.forEach((value) => {
 	console.log(value.content);
+        // Each value of the array contains the values for a coordinate starting with A0
+        // Increments the cooresponding section by one for each "vote" from the collector
         values[((value.content.toUpperCase().charCodeAt(0) - 65) * 10 + parseInt(value.content.substring(1,2).toUpperCase()))]++;
     })
   
 
+    // Simple algorithm for finding all the largest values in an array
     let biggestVals = [];
     let biggestNum = 0;
 
@@ -33,6 +38,7 @@ module.exports.run = async (client, message, args, level, Discord) => {
         biggestVals.push(i);
     }
 
+    // If more than 1 coordinate space has the same # of votes, randomly chooses one of them as the selected space
     let chosenVal = biggestVals[Math.trunc(Math.random() * biggestVals.length)];
 
     message.channel.send(`Voting is over! The space most voted on was ${String.fromCharCode(Math.trunc(chosenVal / 10) + 65)}${chosenVal % 10} with ${biggestNum} votes.`);
