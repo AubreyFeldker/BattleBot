92f34337a6681512284194b6e6899d896f43e3d0
AubreyFeldker <brf190002@utdallas.edu>
2024-09-02 19:28:21 -0500

    threadCreate updated + moved slash-events back to events

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ for (const folder of commandFolders) {
 	}
 }
 
-const eventsPath = path.join(__dirname, 'slash-events');
+const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
