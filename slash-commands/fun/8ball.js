7b20cbfdff4faae396822dafb4e87dfc1e72b051
AubreyFeldker <brf190002@utdallas.edu>
2024-09-19 10:45:12 -0500

    added 8ball command

---
+++ slash-commands/fun/8ball.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('8ball')
+		.setDescription('Seek the wisdom of the Super Mario Magic 8 Ball™.')
+      .addStringOption(option =>
+        option.setName('question')
+            .setDescription('The question you want a response to')
+            .setRequired(true)
+      ),
+	async execute(interaction, client) {
+      const responses = ["WOWZER, OUTLOOK GOOD",
+        "OH YEAH!",
+        "NO PROBLEM!",
+        "OKIE DOKIE!",
+        "SIGNS SAY YA-HAAA!",
+        "IT'S-A-CERTAIN",
+        "WITHOUT A DOUBT!",
+        "SIGNS POINT TO YES",
+        "AS I SEE IT YES",
+        "YEAH! YEAH! OF COURSE!",
+        "CONCENTRATRATE, LET'S-A-GO",
+        "ASK A BRO AGAIN",
+        "SORRY, I'M-A TIRED",
+        "ALL RIGHT, LET'S-A SEE",
+        "ASK AGAIN LATER",
+        "NO WAY, AMATEUR!",
+        "AS I SEE IT, TOUCH LUCK!",
+        "GAH! GIVE IT UP ALREADY!",
+        "DON'T COUNT ON IT",
+        "MY SOURCES SAY NO"
+      ];
+
+      const message = interaction.options.getString('question');
+      const response = responses[Math.floor(Math.random() * responses.length)];
+
+      interaction.reply(`-# "${message}"\n# \`${response}\``);
+	},
+  category: 'fun',
+  localOnly: false,
+  useAnywhere: true
+};
